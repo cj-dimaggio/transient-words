d9ca31ccacd64dea07cf95a94d3d0c1343874b24
Marek <marekz@gmail.com>
2019-01-26 22:51:21 -0500

    Add a 15 minutes time limit option

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 !function($){
   let defaults = {
     position: "top",
-    time_limits: [3, 5, 10, 20, 30, 60],
+    time_limits: [3, 5, 10, 15, 20, 30, 60],
     word_limits: [75, 150, 250, 500, 1667],
     type: "timed",
     limit: 5,
