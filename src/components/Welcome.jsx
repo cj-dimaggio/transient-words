21dbda4e5765eceda912da51829c9d6fe645a7fb
Manuel Ebert <manuel@1450.me>
2020-12-01 22:34:40 -0600

    Add LDTDL Banner

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import WriteButton from './WriteButton';
 import Space from './Space';
+import classNames from 'classnames';
 import { Link } from "react-router-dom";
 
 
-const Welcome = () => {
-    return (
-      <div className="Welcome">
+const Banner = () => {
+  const [visible, setVisible] = useState(false);
+  const [closed, setClosed] = useState(false);
+
+  const innerClasses = classNames('inner', { closed, visible });
+
+  useEffect(() => {
+    if (!visible) {
+      setTimeout(() => setVisible(true), 500)
+    }
+  },[])
+
+  const handleClose = (e) => {
+    e.preventDefault();
+    setClosed(true)
+  }
+
+  return <div className="Banner">
+    <a className={innerClasses} href="https://www.theleastdangeroustodolist.com?ref=mdwa" target="_blank">
+      <div className="logo" />
+      Want to feel accomplished? Check out&nbsp;<strong>The Least <span className="red">Dangerous</span> To-Do List</strong>.
+      <div className="close" onClick={handleClose}>✚</div>
+      </a>
+  </div>
+}
+
+
+const Welcome = () =>
+  <div className="Welcome">
+    <Banner />
         <Link to="/help" className="navButton helpButton">Help</Link>
         <Space xl />
         <div>
@@ -27,7 +55,6 @@ const Welcome = () => {
         </div>
         <div className="accolades" />
       </div>
-    )
-}
+
 
 export default Welcome;
