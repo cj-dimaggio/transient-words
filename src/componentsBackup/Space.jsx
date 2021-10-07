fbab713e9881d2e3f8f50f44e91fdaaab83a8746
CJ DiMaggio <cdimaggio@spotify.com>
2021-10-07 03:22:42 -0400

    Start cleaning up

--- src/componentsBackup/Space.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import classNames from 'classnames';
-
-const Space = (props) => {
-    const classes = classNames('space', props);
-    return <div className={classes} />
-}
-
-export default Space;
